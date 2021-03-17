2588af7356017ed90d6f21b118ff3d1c0ccf0da9
MasterX01 <aakashsaxena14@gmail.com>
2021-03-17 14:51:02 +0530

    [Akash] ADD. added UC-1 working code

--- src/Email.java
+++ src/Email.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+
 public class Email {
+
     public static void main(String[] args) {
         System.out.println("Welcome to Email Validation program");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter you E-mail: ");
+        String mail = scan.nextLine();
+        System.out.println(mail);
+        String exp = "^[a-zA-Z0-9]*";
+        if(Pattern.matches(exp, mail))
+            System.out.println("Email Valid");
+        else
+            System.out.println("Email Invalid");
     }
 }
