31c8176710894c5006212e7b46cfed77fa41c842
MasterX01 <aakashsaxena14@gmail.com>
2021-03-17 15:02:02 +0530

    [Akash] ADD. added UC-2 working code

--- src/Email.java
+++ src/Email.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-
 public class Email {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Email Validation program");
+    public static void emailCheck() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter you E-mail: ");
         String mail = scan.nextLine();
         System.out.println(mail);
-        String exp = "^[a-zA-Z0-9]*";
+        String exp = "^[a-zA-Z0-9]+[@]+[a-zA-Z]*";
         if(Pattern.matches(exp, mail))
             System.out.println("Email Valid");
         else
             System.out.println("Email Invalid");
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Email Validation program");
+        emailCheck();
     }
 }
